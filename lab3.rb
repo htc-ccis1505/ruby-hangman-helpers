ce7e2d4f799a743e08b6855a01280ba916e8aa34
Mary Mosman <mbMosman@users.noreply.github.com>
2016-11-01 07:18:53 -0500

    Create lab3.rb

---
+++ lab3.rb
@@ -0,0 +1,44 @@
+##############################################################################
+# Make a method to ask for a persons name and return it.
+# If no value is entered, keep asking until a name of at least 2 characters
+# was entered.
+# Before returning the name, make sure that the first letter is capitalized.
+#############################################################################
+
+
+
+
+##############################################################################
+# Make a method to take a list of words and a length and return a new list
+# containing only words that have the specified length.
+# If no words have the specified length, return an empty list.
+#############################################################################
+
+
+
+
+##############################################################################
+# Make a method to take a list of letters and a single letter. Return true
+# if the single letter is found in the list, and false otherwise.
+#############################################################################
+
+
+
+
+##############################################################################
+# Make a method to take a word and a single letter. Return true
+# if the single letter is found in the word, and false otherwise.
+#############################################################################
+
+
+
+
+
+##############################################################################
+# Make a method to take a word and a list of letters, return a new word where
+# any letter in the word that is NOT in the list is replaced with a *.
+# Hint = Put letters or the * into an array, then join them into a word. You
+# need two loops to do this.
+#############################################################################
+
+
